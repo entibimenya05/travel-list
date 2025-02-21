66994799c63c34acab913a3071e25142694186c2
emmanuel <entibimenya03@gmail.com>
2025-02-21 14:49:32 +0000

    rendering the list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 function App() {
   return (
     <div className="App">
@@ -27,10 +33,28 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
-  return <div className="list">List</div>;
+function PackingList({ id }) {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-function List() {}
+function Item({ item }) {
+  return (
+    //conditionally styling: if the item.packed: true return a textDecoration;,if not return the element
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
 function Stats() {
   return (
     <footer className="stats">
